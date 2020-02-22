180a9883fe284323b37696af9f16d87254bc14a3
NIKISHIRO <alex.ruso2011@yandex.ru>
2020-02-22 10:44:12 +0400

    Добавил типы для библиотеки Inventory Api

---
+++ src/ANY/types/invApi/index.d.ts
@@ -0,0 +1,20 @@
+interface Item {
+    key: string;
+    amount: number;
+    data?: object;
+}
+
+interface PlayerMp_InvApi {
+    getInventory(): Item[];
+    setInventory(newInventory: Item[]): boolean;
+    hasItem(itemKey: string): boolean;
+    hasItemWithData(itemKey: string, data: object): boolean;
+    getItemIndex(itemKey: string): number;
+    getItemIndexWithData(itemKey: string, data: object): number;
+    getItemAmount(itemKey: string): number;
+    getItemAmountWithData(itemKey: string, data: object): number;
+    getTotalItemAmount(): number;
+    giveItem(itemKey: string, amount: number, data: object): boolean;
+    useItem(itemIdx: number): boolean;
+    removeItem(itemIdx: number, amount?: number): boolean;
+}
