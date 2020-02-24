099a417f8e49e80f58ee2cdffb9ab8ddb3e600e2
Kittan5 <majl.ru.2014@Mail.ru>
2020-02-24 14:21:42 +0400

    delete console.log

--- src/server/project/DAYZ/carspawn/index.ts
+++ src/server/project/DAYZ/carspawn/index.ts
@@ -4,20 +4,16 @@ import vehicleCoords from './vehicleCoords.json';
 export interface VehicleCoord {
     hash: string; // RageEnums - в типах можно найти ЕНУМЫ для указания этого хэша.
     position: Vector3Mp;
-    rotation: Vector3Mp;
-    color: string;
+    rotation?: Vector3Mp;
+    color?: string;
     description?: string; // Описание, которое можно вывести потом через ф-ю.
 }
-mp.events.addCommand('pos',(p:PlayerMp) => {
-    console.log(p.position)
+mp.events.addCommand('pos',() => {
     Car.saveCar("g65amg", new mp.Vector3(111,111,111), new mp.Vector3(0,0,111), [255,255,255,0,0,0], 'spawn tachka');
 })
 
-mp.events.addCommand('sc', (p:PlayerMp,ft:string,hash:string, rotation, color:string, description:string)=>{
-    console.log('VEH'+vehicleCoords)
+mp.events.addCommand('sc', ()=>{
     vehicleCoords.forEach(car =>{
-        console.log(car.position);
-        console.log(car);
         Car.spawnCar(car.hash,new mp.Vector3(car.position.x,car.position.y,car.position.z), new mp.Vector3(car.rotation.x,car.rotation.y,car.rotation.z),[car.color[0],car.color[1],car.color[2],car.color[3],car.color[4],car.color[5]]);
     })
 })
