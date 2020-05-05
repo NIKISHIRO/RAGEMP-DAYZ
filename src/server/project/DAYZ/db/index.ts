e9cd42879488ffc124707b0fee8170543c0e3826
NIKISHIRO <alex.ruso2011@yandex.ru>
2020-05-05 12:36:52 +0400

    Изменил структуру бд.

--- src/server/project/DAYZ/db/index.ts
+++ src/server/project/DAYZ/db/index.ts
@@ -1,2 +1,9 @@
-import './config';
+import knex from 'knex';
 import './tables';
+const dbConfig = require('./knexfile');
+
+const postgres = knex(dbConfig.development);
+
+export {
+    postgres,
+}
