c6cc1e1a09d0b5ee7f5d5b53ff02168a5714c497
NIKISHIRO <alex.ruso2011@yandex.ru>
2020-05-14 22:00:52 +0400

    sync

--- src/client/out/CEF/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CEFBrowser_1 = require("./CEFBrowser");
-require("./keypress");
-require("./inventory");
-require("./notification");
-exports.Browser = new CEFBrowser_1.CEFBrowser('index.html');
